ae3b8939f16c4feed69e5fd4d59c0c13c3a55992
Nachiket Gokhale <gokhalen@gmail.com>
2020-07-08 08:53:40 -0400

    Changes to be committed: 	modified:   objects_tutorial.py

--- objects_tutorial.py
+++ objects_tutorial.py
@@ -10,7 +10,7 @@
  1)   What is an object?
 
  ans)    An object is a 'thing' that contains data and functions to manipulate 
-         (perform operations on) that data. An object contains data and methods.
+         (perform operations on) that data. An object contains data and methods (functions).
          One can also think of it as a way of organizing data and functions. 
          Objects can also be thought of as a way of keeping data and functions 
          'close' to each other.
@@ -43,17 +43,22 @@
          x-y, and element-wise product x*y and element-wise division x/y. If I tell you that
          x and y are complex numbers, you know how to compute x+y and x-y because the 
          set of complex numbers defines how to perform addition and subtraction for all
-         members of that set.
+         members of that set. In other words, x and y come with properties, defined by the 
+         space they come from which tell you how to perform operations.
 
          Similarly, when objects are defined from a 'class' or created from a 'class' 
          they come with functions which can do things that the objects are supposed to do. 
  
-         So, here, we are defining objects x and y by saying x \in R^2, and y \in R^2.
-         In OOP, as in all other types of programming I know of, we say R^2 x and R^2 y.
-         That defines x and y as members of R^2, or in programming terms the type of x and y
+         In all programming languages I know of, we write:
+
+         'R^2 x' and similarly, 'R^2 y'
+
+         instead of saying x \in R^2, y \in R^2.
+
+         That defines x and y as members of R^2, or in programming terms, the type of x and y
          is R^2. 
 
-         We also say that x and y are 'instances' of the 'class' R2. We also say we can 
+         We can also say that x and y are 'instances' of the 'class' R2. We also say we can 
          'instantiate' R2 to get x and y.
 
          An object has 'behaviors' which means we can control how things happen when
@@ -80,13 +85,13 @@
 
          v4 = v1+v2+v3+v4
 
-         Clearly using addvector will quickly get out of hand. Yes, I know one could write a 
-         function to accept an arbitrary number of arguments, but
+         Clearly using the function addvector will quickly get out of hand. 
+         Yes, I know we could write a function to accept an arbitrary number of arguments, but
 
          v4 = v1+v2+v3 is still cleaner and more elegant than v4=addvector(v1,v2,v3) 
          
-         Note: v4=v1+v2+v3 needs a compiler/interpreter smart enough to understand that 
-         the result of v1+v2 is a vector, create that temporary vector v1+v2 and then add
+         Note: v4=v1+v2+v3 needs a compiler/interpreter smart enough to evaluate v1+v2, 
+         understand that the result is a vector, create that temporary vector v1+v2 and then add
          v3 to it. But this is the domain of compiler engineers and I know nothing more 
          of this.  
 
@@ -117,7 +122,7 @@
          But Python knows that 'a' is another name/label for '1'. So it draws the conclusion that 'b' is another name/label for '1'.
          Similarly, it draws the conclusion that 'c' is another name/label for '1'. Variables are nothing but labels/names for objects.
 
-         If you really want to take the object (data and methods) that are labelled 'a' and make a copy, and have that 
+         If you really want to take the object (data and methods) that are labelled 'a' and make a separate copy in memory, and have that 
          copy labelled 'b' you have to do it explicitly. I won't say how, it will distract from the main issue which is learning 
          about objects. 
 
@@ -137,7 +142,7 @@
 So, let's start. 
 
 Use Python 3.7 or better. Put 'breakpoint()' where you want to stop the code and get a debugger prompt. You
-should be able to perform various python operations at the debugger prompt. Use 'continue' or 'c' to proceed. You can also define 
+should be able to perform various Python operations at the debugger prompt. Use 'continue' or 'c' to proceed. You can also define 
 variables at the debugger prompt. Make sure that your variable names do not conflict with debugger commands. So, writing 'b=2'
 on the debugger prompt is not a good idea, because 'b' is a debugger command which sets breakpoints. 'b=2' will signal to the 
 debugger that you're trying to do something with breakpoints. One way to make your variable names not conflict with the debugger 
@@ -145,30 +150,35 @@ commands is to start with your initials or some other unique identifier. You mig
 'c=2' on the debugger prompt is not a good idea because 'c' means 'continue' to the debugger.
 
 There are many things I haven't covered here. Inheritance, encapsulation, difference between class and instance variables,
-classmethods and staticmethods 
+classmethods and staticmethods are things which come to mind. 
  
 '''
 
 # Let's define a function to add two vectors. I won't define functions to subtract, elementwise multiply and divide
-# Defining an excessive number of functions will distract.
+# Defining an excessive number of functions will distract from the goal of learning about objects.
 
 def vector_add(v1,v2):
     '''
     v1 and v2 are lists of length 2
-    lists are like 1-d arrays using v1[0] gets the first element, v1[1] gets the second element
+    lists are like 1-d arrays using 'v1[0]' gets the first element, 'v1[1]' gets the second element
     indexing starts at 0
 
     note: we have not declared the data type for the elements of v1 and v2. they could be integers,floats,
     complex numbers or even strings. As long as the operation '+' is defined for whatever datatype we're passing in
-    this code will work. As you can see, we don't need different functions to add different data types
+    this code will work. 
+
+    As you can see, we don't need different functions to add different data types. 
+    This is a good thing and a bad thing.
 
     '''
+#   add the x components, then the y-components   
     x  = v1[0] + v2[0]
     y  = v1[1] + v2[1]
+
+#   create a list and return it. [x,y] makes a list out of x and y.
     v3 = [x,y]            
     return v3
     
-
 # Let's define two vectors v1 and v2 and get a third v3. Using [] defines a 'list' in Python.
 v1 = [1,2]
 v2 = [3,4]
@@ -215,10 +225,11 @@ v2.y = 4
 # what have we done in the above four lines? we have created variables x and y and they're stored inside v1 and v2
 # Python goes inside the objects v1 and v2 and create variables named x and y. 
 # python is a bit unusual in that it lets you define variables inside v1 and v2 without declaring them first.
+# I call this the ability to define variables dynamically
+# Other langauges like C++ don't let you do so.
 # v1 and v2 have their own copies of x and y.
 # x and y are called instance variables
-# I call this the ability to define variables dynamically
-# Other langauges like C++ don't let you do so. Let's look at the variables we defined.
+# Let's look at the variables we defined.
 
 print('v1.x= ',v1.x, 'v1.y= ',v1.y)
 print('v2.x= ',v2.x, 'v2.y= ',v2.y)
@@ -240,11 +251,11 @@ class Vector():
 
 # we have added a method named __init__ to the class definition. Any method starting and ending with double underscores '__'
 # is a special method in Python. They are also called 'magic methods'.
-# The special property of the __init__ method is that it is, for all practical purposes,
-# the first method to be called after the class is instantiated. The method __init__, in our case, takes 3 arguments
-# 'self' is the instance of an object. 'x' and 'y' are user supplied arguments.
-# We saw earlier that 'v1=Vector()' was enough to instantiate
-# the class, allocate memory for it and create an object. You can think of 'self' as this object,
+# The special property of the __init__ method is that it is, for now,
+# the first method to be called (automatically) after the class is instantiated. The method __init__, in our case, takes 3 arguments
+# 'self' is the instance of an object. 'a' and 'b' are user supplied arguments.
+# We saw earlier that an empty class definition, consisting of only the keyword 'pass', was enough to instantiate
+# the class, allocate memory for it and create an object. You can think of 'self' as this empty object,
 # For methods residing inside a class, the 'self' object gets passed automatically. You don't need to worry about it.
 # We can see this below:
 
@@ -256,6 +267,7 @@ v2 = Vector(9,10)
 # instead of calling Vector() without any arguments, as we did before, we have called it with two arguments.
 # the arguments (7,8) and (9,10) are passed to the __init__ method. They become become the second and third arguments of __init__
 # the first argument being self, the object itself.
+# Let us check that what's inside the object is what we put in.
 
 print('v1.x= ',v1.x, 'v1.y= ',v1.y)
 print('v2.x= ',v2.x, 'v2.y= ',v2.y)
@@ -272,16 +284,19 @@ class Vector():
 
     def addvector(self,v):
 #   the method is adding a vector v to itself and returning the resulting vector
-#   notice the return statement - it is creating a vector using Vector(xx,yy) and returning that newly created vector        
+#   notice the return statement - it is creating a vector using Vector(xx,yy) and returning that newly created vector
+#   in some sense, this definition is recursive. This definition of the method addvector is returning an object of the
+#   same class in which it resides.
         xx = self.x + v.x
         yy = self.y + v.y
         return Vector(xx,yy)
         
-# Let's test it out
+# Let's test it out. First create two vectors
 v1 = Vector(7,8)
 v2 = Vector(9,10)
+
 # here we're calling the addvector method which resides in v1. Notice we called it with only one argument: v2.
-# but we defined it using two arguments (self,v). The first argument 'self' is automatically passed. 
+# but we defined it using two arguments: (self,v). The first argument 'self' is automatically passed. 
 v3 = v1.addvector(v2)
 
 # you can see v3.x = 16 and v3.y = 18
@@ -290,7 +305,7 @@ print('v3.x= ',v3.x, 'v3.y= ',v3.y)
 breakpoint()
 
 # can we add 3 vectors using this approach?
-# v4 = v1 + v2 + v3 + v4
+# v4 = v1 + v2 + v3
 v1 = Vector(7,8)
 v2 = Vector(9,10)  
 v3 = Vector(11,12)
@@ -312,7 +327,6 @@ breakpoint()
 # let's do better. We want to be able to write v4 = v1 + v2 + v3 + ..v_n
 # the way to do it is to add a 'magic method' called __add__
 # redefine the Vector class
-
 
 class Vector():
     def __init__(self,a,b):
@@ -338,7 +352,7 @@ v3 = Vector(11,12)
 
 v4 = v1 + v2 + v3
 
-# The Python interpreter does v1+v2, stores the result in a temporary vector, and then adds v3 to that temporary vector
+# The Python interpreter performs v1+v2, stores the result in a temporary vector, and then adds v3 to that temporary vector
 
 print('v4.x= ',v4.x, 'v4.y= ',v4.y)
 
