6ecb778d55f7e9cb5ca17e70ab2254de227e3772
Nachiket Gokhale <gokhalen@gmail.com>
2020-07-30 18:34:51 +0530

    Changes to be committed: 	new file:   Python_notes/iterator_unpacking_for_arguments.py

---
+++ Python_notes/iterator_unpacking_for_arguments.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 30 13:39:53 2020
+
+@author: aa
+"""
+
+def ff(a,b,c):
+    print(a,b,c)
+    
+tt=(1,2,3)
+
+# can think of *tt evaluating to 1,2,3 with the ","
+# so that the arguments the arguments are mapped corectly
+ff(*tt)
+
+
+aa=[4]
+bb=[5]
+cc=[6]
+
