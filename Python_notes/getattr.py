f781fc93f003d7e45ab21cb59fad28eead1fc2fa
Nachiket Gokhale <gokhalen@gmail.com>
2021-06-03 14:36:37 +0530

    Changes to be committed: 	new file:   Python_notes/getattr.py

---
+++ Python_notes/getattr.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  3 14:19:40 2021
+
+@author: aa
+"""
+
+# Beazley and Jones, 'Python Cookbook' pg. 308
+
+class A:
+    def spam3(self,x,y,z):
+        print('A.spam3 ',x,y,z)
+        
+    def spam(self,x):
+        print('A.spam ',x)
+        
+        
+class B:
+    def __init__(self):
+        self._a = A()
+        
+    def __getattr__(self,name):
+        # First, the __getattr__() method is actually a fallback method that only gets called when
+        # an attribute is not found.
+        # the name argument is actually the name of the attribute to get
+        # in the following examples name would be 'spam' or 'spam3'
+        print('name=',name)
+        return getattr(self._a,name)
+    
+b = B()
+b.spam(11)
+b.spam3(11,12,13)
