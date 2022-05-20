1a1eb078c8fc4492a46b2bb79836b8a204a1690b
Nachiket Gokhale <gokhalen@gmail.com>
2022-05-20 12:59:11 +0530

    Changes to be committed: 	modified:   TeX_ML/ei_ml_arxiv/ei_ml_arxiv2.tex 	new file:   TeX_ML/learn_svd_compare_ng_iisc.py 	modified:   TeX_ML/modelreduction_pca_pod_svd.tex

---
+++ TeX_ML/learn_svd_compare_ng_iisc.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 20 11:28:20 2022
+
+@author: Nachiket Gokhale
+"""
+
+import numpy as np
+
+mm = 4  # number of data points
+nn = 5  # number of features
+kk = 2  # number of columns to use for projection
+
+# Andrew Ng's method
+
+xx     = np.random.random(size=(mm,nn))
+x_mean = np.mean(xx,axis=1).reshape((mm,1))
+XX     = xx - x_mean
+
+Sig_ng = (1.0/mm)*XX.T@XX
+
+
+U_ng,S_ng,VT_ng = np.linalg.svd(Sig_ng)
+S_ng = np.diag(S_ng)
+
+proj_ng = U_ng[:,0:kk]
+
+z_ng    = proj_ng.T@XX.T
+
+
+# IISc 
+# Last part of Matrix_Decompositions pdf
+z_iisc = XX@VT_ng.T[:,0:kk]
+
+print('z_ng and z_iisc are transposes of each other')
+print('z_ng=',z_ng)
+print('z_iisc=',z_iisc)
+
+# the following lines are equivalent
+# proj_ng.T@XX[0,:]
+# XX[0,:]@VT_ng.T[:,0:kk]
